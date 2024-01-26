6fba9b47708590d412486235ee73e3373ebcc1f0
MixusMaxus <mikinikitovic99@gmail.com>
2024-01-26 17:43:13 +0100

    Gotova druga faza nadam se

---
+++ src/rs/ac/bg/etf/pp1/RuleVisitor.java
@@ -0,0 +1,36 @@
+package rs.ac.bg.etf.pp1;
+
+import org.apache.log4j.Logger;
+import rs.ac.bg.etf.pp1.ast.*;
+
+public class RuleVisitor extends VisitorAdaptor{
+
+	int printCallCount = 0;
+	int varDeclCount = 0;
+	
+	Logger log = Logger.getLogger(getClass());
+
+	public void visit(VariableName Variable) { 
+		varDeclCount++;
+	}
+    /*public void visit(Variables Variables) { 
+    	varDeclCount++;
+    }*/
+	
+    public void visit(StatementPrintIme print) {
+		printCallCount++;
+		log.info("Nadjen print");
+	}
+
+    int formalParametersCount=0;
+    /*public void visit(OneFormalParameter OneFormalParameter) {
+    	formalParametersCount++;
+    }
+    public void visit(FormalParameters FormalParameters) { 
+    	formalParametersCount++;
+    }*/
+    
+    /*public void visit(OneParameter OneParameter) { 
+    	formalParametersCount++;
+    }*/
+}
---
+++ test/rs/ac/bg/etf/pp1/MJParserTest.java
@@ -0,0 +1,68 @@
+package rs.ac.bg.etf.pp1;
+
+import java.io.BufferedReader; 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import java_cup.runtime.Symbol;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+import rs.ac.bg.etf.pp1.ast.Program;
+import rs.ac.bg.etf.pp1.util.Log4JUtils;
+/*import rs.etf.pp1.symboltable.Tab;*/
+
+public class MJParserTest {
+
+	static {
+		DOMConfigurator.configure(Log4JUtils.instance().findLoggerConfigFile());
+		Log4JUtils.instance().prepareLogFile(Logger.getRootLogger());
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		Logger log = Logger.getLogger(MJParserTest.class);
+		
+		Reader br = null;
+		try {
+			File sourceCode = new File("test/test302.mj");  //testovi/test1sveprovere.mj
+			log.info("Compiling source file: " + sourceCode.getAbsolutePath());
+			
+			br = new BufferedReader(new FileReader(sourceCode));
+			Yylex lexer = new Yylex(br);
+			
+			MJParser p = new MJParser(lexer);
+	        Symbol s = p.parse();  //pocetak parsiranja
+			//Symbol s= p.debug_parse();
+	        
+	        Program prog = (Program)(s.value);
+			// ispis sintaksnog stabla
+			log.info(prog.toString(""));
+			log.info("===================================");
+
+			// ispis prepoznatih programskih konstrukcija
+			RuleVisitor v = new RuleVisitor();
+			prog.traverseBottomUp(v); 
+	      
+			log.info(" Print count calls = " + v.printCallCount);
+			
+			log.info(" Print variable decls = " + v.varDeclCount);
+
+			//log.info(" Global declarations count = " + v.varDeclCount);
+			
+			//	log.info(" Formal parameters count = " + v.formalParametersCount);
+			
+			//Tab.dump();
+		} 
+		finally {
+			if (br != null) try { br.close(); } catch (IOException e1) { log.error(e1.getMessage(), e1); }
+		}
+
+	}
+	
+	
+}
